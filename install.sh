230d5b9d13815506bd4005532af81bf4a6e2c2f1
奚玉帆 <568875138@qq.com>
2022-04-29 04:39:06 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ downloadCtfd(){
     echo "Download Ctfd"
     git clone --depth 1 -b 3.4.3 https://github.com/CTFd/CTFd .
     git clone --depth 1 https://github.com/frankli0324/ctfd-whale CTFd/plugins/ctfd-whale
+    git clone --depth 1 https://github.com/liuxin2020/ctfd-plugin-multichoice CTFd/plugins/ctfd-plugin-multichoice
     echo "flask_apscheduler" >> requirements.txt
     curl -fsSL https://cdn.jsdelivr.net/gh/frankli0324/ctfd-whale/docker-compose.example.yml -o docker-compose.yml
 }
@@ -25,10 +26,22 @@ configureCtfd(){
     echo "Configure Ctfd"
     sed -i 's/http:\/\/frpc:7400/http:\/\/frank:qwer@frpc:7000/g' /opt/ctfd/CTFd/plugins/ctfd-whale/utils/setup.py
     sed -i 's/ctfd_frp-containers/ctfd_containers/g' /opt/ctfd/CTFd/plugins/ctfd-whale/utils/docker.py
-    # TODO
-    # read -p "enter node domain: (e.g. node1.example.com)"  domain
-    # read -p "enter node http mode port: (e.g. 8080)"  httpPort
-    # read -p "enter node direct mode port range: (e.g. 10000-10100)"  directPort
+    read -p "Enter node domain [127.0.0.1.nip.io]:"  domain
+    [ "${domain}" == "" ] && { domain="127.0.0.1.nip.io"; }
+    sed -i "s/127.0.0.1.nip.io/${domain}/g" /opt/ctfd/docker-compose.yml
+    sed -i "s/127.0.0.1.xip.io/${domain}/g" /opt/ctfd/CTFd/plugins/ctfd-whale/utils/setup.py
+    sed -i "s/\"127.0.0.1\"/\"${domain}\"/g" /opt/ctfd/CTFd/plugins/ctfd-whale/utils/setup.py
+    sed -i "s/\"127.0.0.1\"/\"${domain}\"/g" /opt/ctfd/CTFd/plugins/ctfd-whale/utils/routers/frp.py
+    read -p "Enter node http mode port [8080]:"  httpPort
+    [ "${httpPort}" == "" ] && { httpPort="8080"; }
+    sed -i "s/8080:8080/${httpPort}:8080\n      - 10000-10100:10000-10100/g" /opt/ctfd/docker-compose.yml
+    sed -i "s/8080/${httpPort}/g" /opt/ctfd/CTFd/plugins/ctfd-whale/utils/setup.py
+    read -p "Enter node direct mode port range [10000-10100]:"  directPort
+    [ "${directPort}" == "" ] && { directPort="10000-10100"; }
+    sed -i "s/10100/${directPort#*-}/g" /opt/ctfd/docker-compose.yml
+    sed -i "s/10000/${directPort%-*}/g" /opt/ctfd/docker-compose.yml
+    sed -i "s/10100/${directPort#*-}/g" /opt/ctfd/CTFd/plugins/ctfd-whale/utils/setup.py
+    sed -i "s/10000/${directPort%-*}/g" /opt/ctfd/CTFd/plugins/ctfd-whale/utils/setup.py
 }
 
 runCtfd(){
